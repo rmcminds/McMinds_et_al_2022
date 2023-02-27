5a0a3ece08bb0649ab016d877b822504b4480d8d
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2023-02-27 12:19:07 -0500

    stringtie loop

--- 01_generate_counts.sh
+++ 01_generate_counts.sh
@@ -11,6 +11,7 @@ species=(callithrix_jacchus homo_sapiens macaca_mulatta microcebus_murinus papio
 spec=${species[$SLURM_ARRAY_TASK_ID]}
 
 ref_dir=outputs/primates_20230224/00_references
+out_dir=outputs/primates_20230224/01_generate_counts
 
 module purge
 module load apps/samtools/1.3.1
@@ -20,12 +21,12 @@ module load apps/stringtie/1.3.4b
 for fwd in raw_data/20221215_primate_allometry/fastqs/*_${spec}*_R1_001.fastq.gz; do
 
     rev=${fwd%_R1_001.fastq.gz}_R2_001.fastq.gz
-    sample=${fwd%%_*}
+    sample=$(basename ${fwd%%_*})
 
     # align reads to reference genome with HISAT2, and sort and convert output to bam
-    hisat2 -p 20 -x ${spec}_index --dta-cufflinks -1 ${fwd} -2 ${rev} --summary-file ${sample}_summary.txt | samtools view -@ 20 -bS - | samtools sort -@ 20 - > ${sample}.bam
+    hisat2 -p 20 -x ${ref_dir}/${spec}_index --dta-cufflinks -1 ${fwd} -2 ${rev} --summary-file ${out_dir}/${sample}_summary.txt | samtools view -@ 20 -bS - | samtools sort -@ 20 - > ${out_dir}/${sample}.bam
 
     # calculate transcript abundances with StringTie
-    stringtie -p 20 -B -G ${ref_dir}/${spec^}.*.gff3 -o ${sample}_transcripts.gtf -A ${sample}_abundances.tsv -l ${sample} ${sample}.bam
+    stringtie -p 20 -B -G ${ref_dir}/${spec^}.*.gff3 -o ${out_dir}/${sample}_transcripts.gtf -A ${out_dir}/${sample}_abundances.tsv -l ${sample} ${out_dir}/${sample}.bam
 
 done
