3acc2a427bf1ae737687f6e85d713de4403784dc
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2023-03-04 16:45:35 -0500

    bugfix

--- 00_references.sh
+++ 00_references.sh
@@ -10,51 +10,41 @@
 mkdir -p outputs/primates_20230304/00_references
 cd outputs/primates_20230304/00_references
 
-(
-
-    ncbispec=(daubentonia_madagascariensis lemur_catta sapajus_appella)
-    ncbiacc=(GCA_023783475.1 GCF_020740605.2 GCF_009761245.1)
-
-    module purge
-    module load hub.apps/anaconda3
-    source activate ncbi_datasets
-
-    datasets download genome accession ${ncbiacc[@]} --dehydrated --no-progressbar --include genome
-
-    unzip ncbi_dataset.zip
-    rm ncbi_dataset.zip
-
-    datasets rehydrate --gzip --directory ./
-
-    conda deactivate
-    module purge
-    module load apps/hisat2/2.1.0
-    for i in 0 1 2; do
-
-      (
-          filename=$(basename ncbi_dataset/data/${ncbiacc[$i]}/*.fna.gz)
-
-          cp ncbi_dataset/data/${ncbiacc[$i]}/*.fna.gz ${ncbispec[$i]^}_${filename/fna.gz/fa.gz}
-
-          ## genomes seem to need to be unzipped for hisat2 indexing but can be stored zipped after that
-          zcat ${ncbispec[$i]^}_${filename/fna.gz/fa.gz} > ${ncbispec[$i]}_tmp.fa
-
-          ## build genome index
-          hisat2-build -p 5 ${ncbispec[$i]}_tmp.fa ${ncbispec[$i]}_index
-          
-          rm ${ncbispec[$i]}_tmp.fa
-      ) &
-      
-    done
-    
-    wait
-
-    rm -r ncbi_dataset
-    
-) &
+ncbispec=(daubentonia_madagascariensis lemur_catta sapajus_appella)
+ncbiacc=(GCA_023783475.1 GCF_020740605.2 GCF_009761245.1)
 
 module purge
+module load hub.apps/anaconda3
+source activate ncbi_datasets
+
+datasets download genome accession ${ncbiacc[@]} --dehydrated --no-progressbar --include genome
+
+unzip ncbi_dataset.zip
+rm ncbi_dataset.zip
+
+datasets rehydrate --gzip --directory ./
+
+conda deactivate
+module purge
 module load apps/hisat2/2.1.0
+for i in 0 1 2; do
+
+  (
+      filename=$(basename ncbi_dataset/data/${ncbiacc[$i]}/*.fna.gz)
+
+      cp ncbi_dataset/data/${ncbiacc[$i]}/*.fna.gz ${ncbispec[$i]^}_${filename/fna.gz/fa.gz}
+
+      ## genomes seem to need to be unzipped for hisat2 indexing but can be stored zipped after that
+      zcat ${ncbispec[$i]^}_${filename/fna.gz/fa.gz} > ${ncbispec[$i]}_tmp.fa
+
+      ## build genome index
+      hisat2-build -p 5 ${ncbispec[$i]}_tmp.fa ${ncbispec[$i]}_index
+      
+      rm ${ncbispec[$i]}_tmp.fa
+  ) &
+  
+done
+
 for spec in callithrix_jacchus homo_sapiens macaca_mulatta microcebus_murinus papio_anubis pongo_abelii; do
 
   (
@@ -76,3 +66,7 @@ for spec in callithrix_jacchus homo_sapiens macaca_mulatta microcebus_murinus pa
   ) &
 
 done
+
+wait
+
+rm -r ncbi_dataset
